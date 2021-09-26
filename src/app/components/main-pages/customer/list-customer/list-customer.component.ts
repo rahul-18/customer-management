320101d972d397d54646276417072e51a8320d46
rahul-18 <2009rahul18@gmail.com>
2021-09-26 10:45:45 +0530

    list view and gr

--- src/app/components/main-pages/customer/list-customer/list-customer.component.ts
+++ src/app/components/main-pages/customer/list-customer/list-customer.component.ts
@@ -1,5 +1,6 @@
+import { stringify } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'cm-list-customer',
   templateUrl: './list-customer.component.html',
@@ -9,7 +10,35 @@ export class ListCustomerComponent implements OnInit {
 
   constructor() { }
 
+  customerForm = new FormGroup({
+    customerName: new FormControl(''),
+    gender: new FormControl(''),
+    customerCompName: new FormControl(''),
+    customerCompAdd: new FormControl(''),
+  });
+
+  customers = [];
+
   ngOnInit(): void {
+    if (localStorage.getItem('customers') && localStorage.getItem('customers').length !== 0) {
+      this.customers = JSON.parse(localStorage.getItem('customers'));
+    } else {
+      localStorage.setItem('customers', JSON.stringify([]));
+
+    }
   }
 
+  onSubmit() {
+    let customer =
+    {
+      customerName: this.customerForm.value.customerName,
+      gender: this.customerForm.value.gender,
+      customerCompName: this.customerForm.value.customerCompName,
+      customerCompAdd: this.customerForm.value.customerCompAdd,
+    }
+    console.log(customer);
+    this.customers.push(customer);
+    localStorage.removeItem('customers');
+    localStorage.setItem('customers', JSON.stringify(this.customers));
+  }
 }
--- src/app/main-app-module/main-app-module.module.ts
+++ src/app/main-app-module/main-app-module.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { ListCustomerComponent } from '../components/main-pages/customer/list-customer/list-customer.component';
 import { CreateCustomerComponent } from '../components/main-pages/customer/customer-follow-up/create-customer/create-customer.component';
 import { MainNavComponent } from '../components/shared-comp/main-nav/main-nav.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [ListCustomerComponent, CreateCustomerComponent, MainNavComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ]
 })
 export class MainAppModuleModule { }
